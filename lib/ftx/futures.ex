77ba36cc7c8f95dabede3988a6e605bfdb34c5b8
Hugo <hpjm37@gmail.com>
2022-09-08 14:28:41 +0100

    Format

--- lib/ftx/futures.ex
+++ lib/ftx/futures.ex
@@ -4,9 +4,9 @@ defmodule FTX.Futures do
   def get_futures do
     @base_path
     |> String.trim_trailing("/")
-    |> FTX.build_url
-    |> HTTPoison.get
-    |> FTX.handle_response
+    |> FTX.build_url()
+    |> HTTPoison.get()
+    |> FTX.handle_response()
   end
 
   def get_future(future) when is_binary(future) do
@@ -18,8 +18,8 @@ defmodule FTX.Futures do
 
   def get_funding_rate(future) when is_binary(future) do
     "/funding_rates"
-    |> FTX.build_url
-    |> HTTPoison.get([], [params: [future: future]])
-    |> FTX.handle_response
+    |> FTX.build_url()
+    |> HTTPoison.get([], params: [future: future])
+    |> FTX.handle_response()
   end
 end
