6cc04eb87baaca74bbedd1cfe6f6ea15eb2a01ed
Hugo <hpjm37@gmail.com>
2022-09-08 13:58:49 +0100

    Init commit

---
+++ lib/ftx.ex
@@ -0,0 +1,18 @@
+defmodule FTX do
+  @moduledoc """
+  Documentation for `FTX`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FTX.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ftx_test.exs
@@ -0,0 +1,8 @@
+defmodule FTXTest do
+  use ExUnit.Case
+  doctest FTX
+
+  test "greets the world" do
+    assert FTX.hello() == :world
+  end
+end
