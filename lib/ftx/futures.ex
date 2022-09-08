37f93549e397c4c959ca3112619c9753773ebb6d
Hugo <hpjm37@gmail.com>
2022-09-08 14:28:36 +0100

    get_futures/0

--- lib/ftx/futures.ex
+++ lib/ftx/futures.ex
@@ -1,6 +1,14 @@
 defmodule FTX.Futures do
   @base_path "/futures/"
 
+  def get_futures do
+    @base_path
+    |> String.trim_trailing("/")
+    |> FTX.build_url
+    |> HTTPoison.get
+    |> FTX.handle_response
+  end
+
   def get_future(future) when is_binary(future) do
     (@base_path <> future)
     |> FTX.build_url()
