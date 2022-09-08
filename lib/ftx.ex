1e4fad9c13f239dbc273c65d55be944e97383af9
Hugo <hpjm37@gmail.com>
2022-09-08 14:36:04 +0100

    Maybe alter status code

--- lib/ftx.ex
+++ lib/ftx.ex
@@ -9,17 +9,24 @@ defmodule FTX do
     if json?(headers) do
       case Jason.decode(body) do
         {:ok, decoded_body} ->
-          {status, %HTTPoison.Response{response | body: decoded_body}}
+          maybe_correct_status({status, %HTTPoison.Response{response | body: decoded_body}})
 
         {:error, reason} ->
           {:error, {:decode_error, reason, {status, response}}}
       end
     else
-      {status, response}
+      maybe_correct_status({status, response})
     end
   end
 
   def handle_response({:error, %HTTPoison.Error{}} = error), do: error
+
+  defp maybe_correct_status({:ok, %HTTPoison.Response{status_code: status_code} = response})
+       when status_code not in 200..299 do
+    {:error, response}
+  end
+
+  defp maybe_correct_status({status, response}), do: {status, response}
 
   defp json?(headers) do
     Enum.find_value(headers, false, fn {key, value} ->
